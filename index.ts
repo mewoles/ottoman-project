41e1ea2f0b51cbb3e1266c47cffbf612211ab48a
mewoles <47147257+mewoles@users.noreply.github.com>
2024-09-29 23:50:49 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,15 @@
+import bear from "./bear.png";
+import coin from "./coin.png";
+import highVoltage from "./high-voltage.png";
+import ottoman from "./ottoman.png";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+
+export {
+    bear,
+    coin,
+    highVoltage,
+    ottoman,
+    rocket,
+    trophy
+}
